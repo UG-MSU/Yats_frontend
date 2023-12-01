3b19fd79c24d0b63fb60377a86a74f9a59955a31
Aza <arthurhastings56@gmail.com>
2023-12-01 19:52:58 +0300

    verstka contests

--- src/Pages/Contests.js
+++ src/Pages/Contests.js
@@ -2,7 +2,10 @@ import Registration from "../Components/Registrat"
 import { Navbar, Nav, Container, Form, Button, Row, Col } from 'react-bootstrap'
 import '../Styles/Contests.css';
 import '../Styles/index.css';
+import Poisk from '../Components/Poisk'
+
 const URL = 'http://127.0.0.1:8000/contest/user-contests/'
+
 
 function Contests() {
     async function getData() {
@@ -32,34 +35,80 @@ function Contests() {
     const data = getData();
 
     return(
-        <div className = "body">
-
+        <div className="body">
+        <div className="main-container">
 
             <div className = "my-contests">
 
-                <h2 className="header">Мои контесты</h2>
-                <div className="her">
-                    <div className = "contests">
-                        <div className = "contest">
-                            <div className = "contest-name">Contest name</div>
+                <div className="header">
+                    <h2 className="header-title">Мои контесты</h2>
+                    <Poisk/>
+                </div>
+                <div className="content">
+                    <div className="contests">
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
                         </div>
-                        <div className = "contest">
-                            <div className = "contest-name">Contest name</div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
                         </div>
-                        <div className = "contest">
-                            <div className = "contest-name">Contest name</div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
+                        </div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
+                        </div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
+                        </div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
                         </div>
                     </div>
 
                     <div className="buttons">
 
-                        <div>Регистрация на контест</div>
-                        <div>Создать контест</div>
+                        <div className="button">Регистрация на контест</div>
+                        
+                        <div className="button">Создать контест</div>
 
                     </div>
                 </div>
             </div>
 
+            
+            <div className = "my-contests">
+
+                <div className="header">
+                    <h2 className="header-title">Архив</h2>
+                </div>
+                <div className="content">
+                    <div className="contests">
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
+                        </div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
+                        </div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
+                        </div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
+                        </div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
+                        </div>
+                        <div className="contest">
+                            <div className="contest-name">Contest name</div>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+        
+        </div>
         </div>
         
     )
